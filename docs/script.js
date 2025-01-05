9c2fc6a452103b476361eccfcc8d700520eb809b
Emma Taylor <emmataylor@macbookpro.mynetworksettings.com>
2025-01-05 01:57:41 -0500

    console log fun

--- docs/script.js
+++ docs/script.js
@@ -89,7 +89,68 @@ function resume() {
   }
 }
 
+const messages = [
+    'Not even they can stop me now',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Boy, I be flyin overhead',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Their heavy words cant bring me down',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Boy, Ive been raised from the dead',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'No one even knows how hard life was I dont even think about it now because',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'I finally found you',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Oh, sing it to me',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Now my life is sweet like cinnamon',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Like a fucking dream Im livin in',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Baby, love me cause Im playin on the radio',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'How do you like me now?',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Pick me up and take me like a vitamin',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Cause my bodys sweet like sugar venom, oh, yeah',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Baby, love me cause Im playin on the radio',
+    'Not even they can stop me now',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Boy, I be flyin overhead',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Their heavy words cant bring me down',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Boy, Ive been raised from the dead',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'No one even knows how hard life was I dont even think about it now because',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'I finally found you',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Oh, sing it to me',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Now my life is sweet like cinnamon',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Like a fucking dream Im livin in',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Baby, love me cause Im playin on the radio',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'How do you like me now?',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Pick me up and take me like a vitamin',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Cause my bodys sweet like sugar venom, oh, yeah',
+    '*ੈ✩‧₊˚༺☆༻*ੈ✩‧₊˚',
+    'Baby, love me cause Im playin on the radio'
+];
 
+messages.forEach((message, index) => {
+    setTimeout(() => {
+        console.log(message);
+    }, index * 2000); // 2000 ms = 2 seconds
+});
 
 
 const imageFolder = 'hpimages/';
@@ -149,7 +210,8 @@ function setupSpaceBarListener() {
 window.addEventListener('load', () => {
     loadImages(); 
     setupSpaceBarListener(); 
-});
+}
+)
 
 
 
